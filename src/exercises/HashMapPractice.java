8be25270582170ab2f17b20d5bc70269ef980523
Grace Thompson <Grace.thompson17@aol.com>
2022-04-12 21:20:40 -0500

    hash map not done

--- src/exercises/HashMapPractice.java
+++ src/exercises/HashMapPractice.java
@@ -1,4 +1,12 @@
 package exercises;
 
+import java.util.HashMap;
+
 public class HashMapPractice {
+    public static void main(HashMap<Integer, String> map){
+
+    }
+    public static void main(String[] args){
+
+    }
 }
