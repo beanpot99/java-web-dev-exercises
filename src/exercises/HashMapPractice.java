3479824712c8589326d2ebfab139c23c781f7065
Grace Thompson <Grace.thompson17@aol.com>
2022-04-13 20:49:50 -0500

    all done

--- src/exercises/HashMapPractice.java
+++ src/exercises/HashMapPractice.java
@@ -1,12 +1,22 @@
 package exercises;
 
 import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
 
 public class HashMapPractice {
-    public static void main(HashMap<Integer, String> map){
 
-    }
     public static void main(String[] args){
-
+    HashMap<Integer, String> students= new HashMap<>();
+    Scanner input = new Scanner(System.in);
+    String newStudent;
+    System.out.println("Enter student name: ");
+    newStudent = input.nextLine();
+    System.out.print("Grade: ");
+    Integer newGrade = input.nextInt();
+    students.put(newGrade, newStudent);
+    for(Map.Entry<Integer, String> student: students.entrySet()){
+        System.out.println(student.getKey() + (" ") + student.getValue());
+    }
     }
 }
